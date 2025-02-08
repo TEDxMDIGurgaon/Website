94345261fcef0c4dc6278b8dd486fcc282874eb1
Chrysaor-thegoldsword <ns141099@gmail.com>
2025-02-08 21:25:46 +0530

    SEO; Added Events tab

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -97,7 +97,14 @@ const NavbarComponent = () => {
                 Our Sponsors
               </a>
             </li>
-            
+            <li>
+              <a
+                href="/#services"
+                className="block py-2 px-3 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-400 md:p-0 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Event 4.0
+              </a>
+            </li>
             <li>
               <a
                 href="#footer"
