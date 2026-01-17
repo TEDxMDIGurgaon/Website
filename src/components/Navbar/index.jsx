fa62816ca163e8a17b6a506e45f21925481725f5
Manoj Agarwal <manoj@tpms.co.in>
2026-01-17 16:04:29 +0530

    Update speakers/team images and fix carousel top alignment

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -70,7 +70,7 @@ const NavbarComponent = () => {
                     : "block py-2 px-3 text-gray-400 md:p-0 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-400"
                 }
               >
-                Event 4.0
+                Past Events
               </NavLink>
             </li>
             <li>
